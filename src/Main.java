1f4628aeeed744445eeb72a161a49d98b48af468
katros01 <kairos.mutabaruka@amalitechtraining.org>
2024-06-14 23:48:01 +0200

    check for all numbers meet peak-columns condition in matrix

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
             for(int y = 0; y< numberCol; y++){
 
                 if(!input.hasNextInt()){ // validate inputs
-                    System.out.println("the value you entered on index (" + x + "," + y + ") is not a number!"  );
+                    System.out.println("the value you entered on index (" + x + "," + y + ") is not a number!");
                     input.next();
                 }
 
@@ -20,31 +20,46 @@ public class Main {
     static void findPeakColumn(int row, int col, int[][] a){
 
         for(int i = 0; i< row; i++) {
-            int max = 0;
-            int maxCol = 0;
+            int max = 0;      // stores maximum number in column
+            int maxCol = 0;  // index of maximum number
             int min = 0;
 
-            for(int j = 0; j< col; j++) {
+            for(int j = 0; j< col; j++) {  // check for maximum number in column
                 if(a[i][j] >= max) {
                     max = a[i][j];
                     maxCol = j;
                 }
             }
 
-            for(int x = 0; x< row; x++) {
+            for(int j = 0; j< col; j++) {  // check for another number equals to maximum
+                int m = a[i][j];  //stores minimum in column
+                int mi = 0;       // minimum index
+                if(j!=maxCol && a[i][j] == max) {
+                    for(int x = 0; x < row; x++) {  //check if the maximum in column is minimum in row
+                        if(x == 0){
+                            m = a[x][j];
+                        } else if(a[x][j] <= m) {
+                            m = a[x][j];
+                            mi = j;
+                        }
+                    }
+
+                    if (max == m) {
+                        System.out.println("(" + (i + 1) + "," + (mi + 1) + ")" + " " + "=" + " " + a[i][j]);
+                    }
+                }
+
+            }
+
+            for(int x = 0; x< row; x++) {  //check if the maximum in column is minimum in row
                 if(x == 0){
                     min = a[x][maxCol];
                 } else if(a[x][maxCol] <= min) {
                     min = a[x][maxCol];
                 }
             }
-            System.out.println(max + " " + min);
+
             if (max == min) {
-                for (int j = 0; j< col; j++) {
-                    if(maxCol != j && a[i][j] == max){
-                        System.out.println("(" + (i + 1) + "," + (j + 1) + ")" + " " + "=" + " " + max);
-                    }
-                }
                 System.out.println("(" + (i + 1) + "," + (maxCol + 1) + ")" + " " + "=" + " " + max);
             }
         }
